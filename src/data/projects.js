2bf6e028242419be4748b643ca49cc217fa08807
AntonSynytsia <anton.synytsia@gmail.com>
2020-03-23 01:28:09 -0700

    Added Angular project

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,20 +3,27 @@ const projects = [
     name: 'Pizza Ordering App',
     description:
       'A pizza ordering app built with React, Redux, and Bootstrap 4; contains multiple views for choosing pizza crust type, topology, and entering customer information.',
-    demoUrl: '#',
+    demoUrl: 'https://antonsynytsia.github.io/jump-redux-pizza/index.html',
     learnMoreUrl: 'https://github.com/AntonSynytsia/redux-pizza-jump-2020',
   },
   {
     name: 'JUMP Showcase App',
     description:
       'A full-stack web application designed for handling authentication with Express Session; utilizes MySQL and Sequelize. Includes custom designed and animated Carousel, Gallery, and Todo Apps. Note: The demo only shows the front-end.',
-    demoUrl: '#',
+    demoUrl: 'https://antonsynytsia.github.io/jump-react-showcase/index.html',
     learnMoreUrl: 'https://github.com/AntonSynytsia/showcase-app',
+  },
+  {
+    name: 'My Portfolio',
+    description:
+      "A React and Material UI app, you're currently viewing, designed to showcase my works and a little about myself.",
+    demoUrl: 'https://antonsynytsia.github.io/index.html',
+    learnMoreUrl: 'https://github.com/AntonSynytsia/iprofile',
   },
   {
     name: 'AstroClimb',
     description:
-      'A full-stack web application designed to market my ClothWorks and MSPhysics products. Built with React, Redux, Express, MongoDB/Firestore. Hosted at Google Cloud Platform via Docker. Includes JWT authentication, reCATCHA, and email verification.',
+      'A full-stack web application designed to market my ClothWorks and MSPhysics products. Built with React, Redux, Express, MongoDB/Firestore. Hosted at Google Cloud Platform via Docker. Includes JWT authentication, reCATCHA, and email verification. Note: Repository is private; REST API design is based on devconnectors Udemy course, which is also on my GitHub.',
     demoUrl: 'https://astroclimb.appspot.com/',
     learnMoreUrl: '',
   },
@@ -28,11 +35,11 @@ const projects = [
     learnMoreUrl: 'https://github.com/NathanTan/SeniorCapstone',
   },
   {
-    name: 'My Portfolio',
+    name: 'Angular Dev Logger',
     description:
-      "A React and Material UI app, you're currently viewing, designed to showcase my works and a little about myself.",
-    demoUrl: '#',
-    learnMoreUrl: 'https://github.com/AntonSynytsia/iprofile',
+      'A ToDo logger designed with Angular 8, following a Udemy course.',
+    demoUrl: 'https://antonsynytsia.github.io/angular-logger/index.html',
+    learnMoreUrl: 'https://github.com/AntonSynytsia/clientpanel',
   },
 ];
 export default projects;
